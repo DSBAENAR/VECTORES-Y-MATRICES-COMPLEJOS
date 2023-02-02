f4646a70aa35a46c96ce7e9a5bc90dd96bb54c24
DSBAENAR <68250833+DSBAENAR@users.noreply.github.com>
2023-02-02 11:00:24 -0500

    Commit Tests

---
+++ Pruebas.py
@@ -0,0 +1,106 @@
+import numpy as np
+import unittest
+
+class TestQuantumOperations(unittest.TestCase):
+    def setUp(self):
+        self.complex_vector1 = np.array([1 + 1j, 2 + 2j, 3 + 3j])
+        self.complex_vector2 = np.array([1 + 1j, 2 + 2j, 4 + 4j])
+        self.complex_matrix1 = np.array([[1 + 1j, 2 + 2j], [3 + 3j, 4 + 4j]])
+        self.complex_matrix2 = np.array([[2 + 2j, 1 + 1j], [1 + 1j, 2 + 2j]])
+
+    def test_add_complex_vectors(self):
+        result = add_complex_vectors(self.complex_vector1, self.complex_vector2)
+        expected = np.array([2 + 2j, 4 + 4j, 7 + 7j])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_inverse_complex_vector(self):
+        result = inverse_complex_vector(self.complex_vector1)
+        expected = np.array([-1 - 1j, -2 - 2j, -3 - 3j])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_scalar_multiplication_complex_vector(self):
+        result = scalar_multiplication_complex_vector(2, self.complex_vector1)
+        expected = np.array([2 + 2j, 4 + 4j, 6 + 6j])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_add_complex_matrices(self):
+        result = add_complex_matrices(self.complex_matrix1, self.complex_matrix2)
+        expected = np.array([[3 + 3j, 3 + 3j], [4 + 4j, 6 + 6j]])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_inverse_complex_matrix(self):
+        result = inverse_complex_matrix(self.complex_matrix1)
+        expected = np.array([[-1 - 1j, -2 - 2j], [-3 - 3j, -4 - 4j]])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_scalar_multiplication_complex_matrix(self):
+        result = scalar_multiplication_complex_matrix(2, self.complex_matrix1)
+        expected = np.array([[2 + 2j, 4 + 4j], [6 + 6j, 8 + 8j]])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_matrix_transpose(self):
+        A = np.array([[(2, 1), (3, -2)], [(5, -3), (1, 1)]])
+        B = np.array([[(2, 1), (5, -3)], [(3, -2), (1, 1)]])
+        np.testing.assert_array_equal(matrix_transpose(A), B)
+
+        A = np.array([[(2, 1)], [(3, -2)], [(5, -3)]])
+        B = np.array([[(2, 1), (3, -2), (5, -3)]])
+        np.testing.assert_array_equal(matrix_transpose(A), B)
+
+        A = np.array([[(1, 2)]])
+        B = np.array([[(1, 2)]])
+        np.testing.assert_array_equal(matrix_transpose(A), B)
+
+        def test_conjugate_matrix(self):
+            result = conjugate_matrix(self.complex_matrix1)
+            expected = np.array([[1 - 1j, 2 - 2j], [3 - 3j, 4 - 4j]])
+            np.testing.assert_array_equal(result, expected)
+
+        def test_adjoint_matrix(self):
+            result = adjoint_matrix(self.complex_matrix1)
+            expected = np.array([[1 - 1j, 3 - 3j], [2 - 2j, 4 - 4j]])
+            np.testing.assert_array_equal(result, expected)
+
+        def test_matrix_multiplication(self):
+            result = matrix_multiplication(self.complex_matrix1, self.complex_matrix2)
+            expected = np.array([[6 + 6j, 6 + 6j], [18 + 18j, 18 + 18j]])
+            np.testing.assert_array_equal(result, expected)
+
+        def test_matrix_vector_action(self):
+            result = matrix_vector_action(self.complex_matrix1, self.complex_vector1)
+            expected = np.array([10 + 10j, 22 + 22j])
+            np.testing.assert_array_equal(result, expected)
+
+        def test_inner_product(self):
+            result = inner_product(self.complex_vector1, self.complex_vector2)
+            expected = 30 + 30j
+            self.assertEqual(result, expected)
+
+        def test_vector_norm(self):
+            result = vector_norm(self.complex_vector1)
+            expected = np.sqrt(14)
+            self.assertAlmostEqual(result, expected, places=9)
+
+        def test_distance_between_vectors(self):
+            result = distance_between_vectors(self.complex_vector1, self.complex_vector2)
+            expected = np.sqrt(2)
+            self.assertAlmostEqual(result, expected, places=9)
+
+        def test_unitary_matrix(self):
+            result = unitary_matrix(self.complex_matrix1)
+            self.assertFalse(result)
+
+        def test_hermitian_matrix(self):
+            result = hermitian_matrix(self.complex_matrix1)
+            self.assertFalse(result)
+
+        def test_tensor_product(self):
+            result = tensor_product(self.complex_vector1, self.complex_vector2)
+            expected = np.array([[1 + 1j, 2 + 2j, 4 + 4j, 6 + 6j],
+                                 [2 + 2j, 4 + 4j, 8 + 8j, 12 + 12j],
+                                 [3 + 3j, 6 + 6j, 9 + 9j, 12 + 12j],
+                                 [6 + 6j, 12 + 12j, 12 + 12j, 24 + 24j]])
+            np.testing.assert_array_equal(result, expected)
+
+    if _name_ == '_main_':
+        unittest.main()
