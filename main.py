419163b7e52ea778cee1e08cc078628ac219dd4a
DSBAENAR <68250833+DSBAENAR@users.noreply.github.com>
2023-02-05 23:46:07 -0500

    Thrid Change
    Just need to complete matrix_vector_action

--- main.py
+++ main.py
@@ -19,24 +19,28 @@ def scalar_multiplication_complex_matrix(c, m):
     return np.multiply(c,m)
 
 def transpose_matrix(m):
-    return np.transpose(m)
+    return [list(x) for x in zip(*m)]
 
 def conjugate_matrix(m):
     return np.conjugate(m)
 
 def adjoint_matrix(m):
-    return np.conjugate(np.transpose(m))
+    return np.conjugate(transpose_matrix(m))
 
 def matrix_multiplication(m1, m2):
+    if len(m1[0]) != len(m2):
+        return 'No se puede realizar la multilplicacion'
     return np.dot(m1, m2)
+    
 
 def matrix_vector_multiplication(m, v):
     return np.dot(m, v)
+
 def inner_product_complex_vectors(v1, v2):
-    return np.dot(np.conjugate(v1), v2)
+    return np.dot(conjugate_matrix(v1), v2)
 
 def norm_complex_vector(v):
-    return np.sqrt(np.dot(np.conjugate(v), v))
+    return np.sqrt(np.dot(conjugate_matrix(v), v))
 
 def distance_complex_vectors(v1, v2):
     return norm_complex_vector(v1 - v2)
