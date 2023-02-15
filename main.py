bc8fc2e23d4707845b9eedf7702a5e91c0c3de32
DSBAENAR <68250833+DSBAENAR@users.noreply.github.com>
2023-02-15 18:57:05 -0500

    Update main.py
    Definitive code

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 def add_complex_vectors(v1, v2):
     return np.add(v1, v2)
 
-def inverse_complex_vector(v):
-    return np.negative(v)
+def sub_complex_vectors(v1,v2):
+    return v1-v2
 
 def scalar_multiplication_complex_vector(c, v):
     return np.multiply(c, v)
@@ -12,14 +12,15 @@ def scalar_multiplication_complex_vector(c, v):
 def add_complex_matrices(m1, m2):
     return np.add(m1, m2)
 
-def inverse_complex_matrix(m):
-    return np.negative(m)
+def inverse_complex_matrix(m1,m2):
+    return m1-m2
 
 def scalar_multiplication_complex_matrix(c, m):
     return np.multiply(c,m)
 
 def transpose_matrix(m):
     return [list(x) for x in zip(*m)]
+
 
 def conjugate_matrix(m):
     return np.conjugate(m)
@@ -29,12 +30,15 @@ def adjoint_matrix(m):
 
 def matrix_multiplication(m1, m2):
     if len(m1[0]) != len(m2):
-        return 'No se puede realizar la multilplicacion'
+        raise TypeError( 'No se puede realizar la multilplicacion')
     return np.dot(m1, m2)
-    
 
 def matrix_vector_multiplication(m, v):
-    return np.dot(m, v)
+    if len(m[0])!=len(v):
+        raise TypeError( 'No se puede realizar la multilplicacion')
+    return np.dot(m,v)
+
+
 
 def inner_product_complex_vectors(v1, v2):
     return np.dot(conjugate_matrix(v1), v2)
@@ -48,8 +52,17 @@ def distance_complex_vectors(v1, v2):
 def is_unitary_matrix(m):
     return np.allclose(np.dot(m, np.conjugate(np.transpose(m))), np.identity(m.shape[0]))
 
+
 def is_hermitian_matrix(m):
     return np.allclose(m, np.conjugate(np.transpose(m)))
 
 def tensor_product(m1, m2):
     return np.kron(m1, m2)
+    
+def Eigen_Values(m):
+    eigen_values=np.linalg.eig(m)
+    return eigen_values[0]
+
+def Eigen_Vectors(m):
+    eigen_vectors=np.linalg.eig(m)
+    return eigen_vectors[1]
